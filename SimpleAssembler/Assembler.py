b3209c06261f6ed452d01c3def98fae67c04925e
krish25271 <krish25271@iiitd.ac.in>
2026-03-07 17:34:03 +0530

    J-type S-type instruct + label saving

--- SimpleAssembler/Assembler.py
+++ SimpleAssembler/Assembler.py
@@ -172,3 +172,144 @@ def utype(s):
     else:
         opcode="0110111"
     return imm+rd+opcode
+
+def Jtype(instr):
+    opcode = '1101111'      #opcode for j-type instr
+
+    lst = instr.split()     #components of instr
+    ra = ''
+    register,label = '',''
+    #commas, space
+    flag = True
+    if (len(lst) != 2) or (lst[0] != "jal") or ("," not in lst[1]) or (len(lst[1].split(','))!=2 ):
+        flag = False
+    else:
+        register, label = lst[1].split(",")
+        if label not in Labels:
+            flag = False
+        else:
+            label_location = Labels[label]
+        if register not in Registers.keys():
+            if register not in Registers.values():
+                flag = False
+            else: pass
+        else:
+            register = Registers[register]      #ABI convention is changed to x{i}
+                    
+    if flag == False:       #if any error occurs, flag is False
+        return flag
+    
+    ra = int(register[1:])          #{i} of x{i} changed to binary
+    ra = str(bin(ra))[2:]
+    while len(ra)<5:                #extend to 5bits
+        ra="0"+ra
+
+    imm_int = label_location - PC           #Immediate Calc
+    imm_str, imm_str_temp = '',''
+    #Sign extension of imm-int into binary of 20 bits
+    if imm_int < 0:
+        imm_str_temp = str(bin(imm_int))[3:]
+        while(len(imm_str_temp)<20):
+            imm_str_temp = '1' + imm_str_temp
+    else:
+        imm_str_temp = str(bin(imm_int))[2:]
+        while(len(imm_str_temp)<20):
+            imm_str_temp = '0' + imm_str_temp 
+    
+    #Format of immediate in j-type is imm-str
+    imm_str = imm_str_temp[-20]+ imm_str_temp[-10:-1]+ imm_str_temp[-1]+ imm_str_temp[-11] + imm_str_temp[-19:-12] + imm_str_temp[-12]
+    
+    bin_instr = imm_str + ra + opcode
+    return bin_instr
+
+def Stype(instr):
+    opcode = '0100011'      #opcode and funct3 values for s-type instr
+    funct3 = '010'
+
+    lst = instr.split()     #components of instr
+    data = ''
+    src =''
+    register, rest = '',''
+    imm_str, imm_bin = '',''
+    #commas, space
+    flag = True
+    if (len(lst) != 2) or (lst[0]!='sw') or ("," not in lst[1]) or (len(lst[1].split(','))!=2):
+        flag = False
+    else:  
+        register, rest = lst[1].split(',')
+        #Register_validity
+        if register not in Registers.keys():
+            if register not in Registers.values():
+                flag = False
+        else:
+            register = Registers[register]      #ABI changed to x{i}
+
+        #imm(src) validity
+        if '(' not in rest or ')' not in rest:      #Parenthesis must be closed
+            flag = False
+        else:
+            open, close = 0,0       #open is index("("), close is index(")")
+            for a in range(len(rest)):
+                if rest[a] == '(':
+                    if a == 0:
+                        flag = False        #if open = 0, imm is not provided
+                    open = a
+                if rest[a] == ')':
+                    close = a
+            if open>close:          #invalid parenthesis
+                flag = False
+            else:
+                imm_str = rest[0:open]
+                src = rest[open+1:close]
+                if src not in Registers.keys():
+                    if src not in Registers.values():
+                        flag = False
+                else:
+                    src = Registers[src]        #ABI changed to x{i}
+
+    if flag == False:
+        return flag
+    
+    data = int(register[1:])
+    data = str(bin(data))[2:]
+    while len(data)<5:      #Bit extension to 5
+        data="0"+data
+    
+    imm_bin = int(imm_str)
+    if imm_bin < 0:     #Sign extension of imm-bin to 12 bits
+        imm_bin = str(bin(imm_bin))[3:]
+        while(len(imm_bin)<12):
+            imm_bin = '1' + imm_bin
+    else:
+        imm_bin = str(bin(imm_bin))[2:]
+        while(len(imm_bin)<12):
+            imm_bin = '0' + imm_bin
+    
+    src = int(src[1:])
+    src = str(bin(src))[2:]
+    while len(src)<5:       #Bit extension to 5
+        src="0"+src
+
+    #Instruction encoding
+    bin_instr = imm_bin[-12:-5]+data+src+funct3+imm_bin[-5:-1]+imm_bin[-1]+opcode
+    return bin_instr
+
+def find_label():
+    global lines
+    temp1=PC
+    for i in range(len(lines)):
+        if ':' in lines[i]:
+            temp = re.split(r"[:]+",lines[i])
+            Labels[temp[0]] = temp1      #Label saved
+
+            for j in range(len(lines[i])):      #j is index(':')
+                if lines[i][j] == ":":
+                    break
+                
+            if temp[0][0].isalpha() and lines[i][j-1] != ' ':   #if first letter is not alphabet or if there is " " bw label and ":",# label invalid
+                Label_validity[temp1] = True
+            else:
+                Label_validity[temp1] = False
+            lines[i] = lines[i][j+2:]       #instruction without labels
+            lines[i] = lines[i].strip()     #remove white spaces
+        temp1+=4
